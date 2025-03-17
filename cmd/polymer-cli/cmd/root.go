61d3085f4b3f3703aa3c6fd4fef83e884bd6052f
stevenlei <contact@stevenlei.com>
2025-03-17 18:18:29 +0800

    add build script

--- cmd/polymer-cli/cmd/root.go
+++ cmd/polymer-cli/cmd/root.go
@@ -23,14 +23,11 @@ Learn more about the Prove API at https://docs.polymerlabs.org`,
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute() error {
 	// Disable the completion command
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
 
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
+	return rootCmd.Execute()
 }
 
 func init() {
